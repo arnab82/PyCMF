de4e761afa743396a8c2b5ee961463829e649e54
arnab82 <arnabbachhar39@gmail.com>
2024-01-18 18:00:06 -0500

    name of rdm functions added only

--- pycmf/cmf.py
+++ pycmf/cmf.py
@@ -188,6 +188,123 @@ class CMF:
         print(" Occupation Numbers:")
         [print("%4i %12.8f"%(i,occs[i])) for i in range(len(occs))]
         print(" FCI:        %12.8f Dim:%6d"%(efci,fci_dim))
+        
+        
+    def make_rdm1s(self, state=None, ncas=None, nelecas=None, root=0):
+        """Returns the aa and bb blocks of the spin 1RDM.
+
+        Parameters
+        ----------
+        state : np.ndarray
+            Unused, but kept to continuity with `mcscf.CASCI`
+        ncas : int
+            Unused, but kept to continuity with `mcscf.CASCI`
+        nelecas : int
+            Unused, but kept to continuity with `mcscf.CASCI`
+        root : int
+            State to read RDM for, default is 0.
+
+        Returns
+        -------
+        tuple
+            A tuple of the aa and bb blocks of the spin 1RDM.
+            E.g. (dm_a, dm_b)
+        """
+        #create 1 spin RDM
+        # dm_ab = 
+        # return (dm_ab[::2, ::2], dm_ab[1::2, 1::2]) 
+        raise NotImplementedError
+    def make_rdm1(self, state, norb, nelec, link_index=None, **kwargs):
+        # Avoid calling self.make_rdm12 because it may be overloaded
+        return self.make_rdm12(state, norb, nelec, link_index, **kwargs)[0]
+    def make_rdm12(self, state, norb, nelec, link_index=None, **kwargs):
+        nelectrons = 0
+        if isinstance(nelec, (int, np.integer)):
+            nelectrons = nelec
+        else:
+            nelectrons = nelec[0] + nelec[1]
+
+        
+        # 2RDMs should be stored here as E2[i1,k1,j2,l2] (for PySCF purposes)
+       
+        two_rdm = np.zeros((norb, norb, norb, norb))
+        #write code here to fill in two_rdm
+        
+        # Symmetry addon
+        # write code here to incorporate symmetry into two_rdm
+
+        # (This is coherent with previous statement about indexes)
+        one_rdm = np.einsum("ikjj->ki", two_rdm)
+        one_rdm /= nelectrons - 1
+        return one_rdm, two_rdm
+    def trans_rdm1(self, statebra, stateket, norb, nelec, link_index=None, **kwargs):
+        return self.trans_rdm12(statebra, stateket, norb, nelec, link_index, **kwargs)[0]
+    def trans_rdm12(self, statebra, stateket, norb, nelec, link_index=None, **kwargs):
+        nelectrons = 0
+        if isinstance(nelec, (int, np.integer)):
+            nelectrons = nelec
+        else:
+            nelectrons = nelec[0] + nelec[1]
+
+        
+        #execute cmf code to get 2RDMs
+
+        # 2RDMs should be stored here as E2[i1,k1,j2,l2] (for PySCF purposes)
+        two_rdm = np.zeros((norb, norb, norb, norb))
+        #write code here to fill in two_rdm
+
+        # (This is coherent with previous statement about indexes)
+        one_rdm = np.einsum("ikjj->ki", two_rdm)
+        one_rdm /= nelectrons - 1
+        return one_rdm, two_rdm
+    def make_rdm123(self, state, norb, nelec, link_index=None, **kwargs):
+        import time
+        if self.has_three_rdm == False:
+            start = time.time()  
+            end= time.time() 
+        #execute cmf code to get 3RDMs
+            print(" computation of RDMs took %10.2f sec" % (end - start))
+          
+
+        # 3RDMs should be stored here as E3[i1,j2,k3,l3,m2,n1] (for PySCF purposes)
+            
+
+        nelectrons = 0
+        if isinstance(nelec, (int, np.integer)):
+            nelectrons = nelec
+        else:
+            nelectrons = nelec[0] + nelec[1]
+        three_rdm = np.zeros((norb, norb, norb, norb, norb, norb))
+    
+        
+
+        # (This is coherent with previous statement about indexes)
+        two_rdm = np.einsum("ijkklm->ijlm", three_rdm)
+        two_rdm /= nelectrons - 2
+        one_rdm = np.einsum("ijjk->ki", two_rdm)
+        one_rdm /= nelectrons - 1
+        return one_rdm, two_rdm, three_rdm
+    def _make_dm123(self, state, norb, nelec, **kwargs):
+        r"""Note this function does NOT compute the standard density matrix.
+        The density matrices are reordered to match the the fci.rdm.make_dm123
+        function (used by NEVPT code).
+        The returned "2rdm" is :math:`\langle p^\dagger q r^\dagger s\rangle`;
+        The returned "3rdm" is :math:`\langle p^\dagger q r^\dagger s t^\dagger u\rangle`.
+        """
+        one_rdm, two_rdm, three_rdm = self.make_rdm123(state, norb, nelec, None, **kwargs)
+        three_rdm = np.einsum("mkijln->ijklmn", three_rdm).copy()
+        three_rdm += np.einsum("jk,lm,in->ijklmn", np.identity(norb), np.identity(norb), one_rdm)
+        three_rdm += np.einsum("jk,miln->ijklmn", np.identity(norb), two_rdm)
+        three_rdm += np.einsum("lm,kijn->ijklmn", np.identity(norb), two_rdm)
+        three_rdm += np.einsum("jm,kinl->ijklmn", np.identity(norb), two_rdm)
+
+        two_rdm = np.einsum("iklj->ijkl", two_rdm) + np.einsum("li,jk->ijkl", one_rdm, np.identity(norb))
+
+        return one_rdm, two_rdm, three_rdm
+    def make_rdm4(self,state,norb,nelec,**kwargs,):
+        # should be written here as E4[i1,j2,k3,l4,p1,o2,n3,m4]
+        # (for PySCF purposes)
+        raise NotImplementedError
 
     def energy(self):
         """
